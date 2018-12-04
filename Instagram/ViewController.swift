72294f63f03010160719aba734943dc039eb86d1
OuSS <ouss.cs@gmail.com>
2018-12-04 22:39:36 +0100

    Registration Screen Created

--- Instagram/ViewController.swift
+++ Instagram/ViewController.swift
@@ -9,11 +9,77 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let addPhotoButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "plus_photo"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let emailTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Email"
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.borderStyle = .roundedRect
+        tf.font = UIFont.systemFont(ofSize: 14)
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        return tf
+    }()
+    
+    let usernameTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Username"
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.borderStyle = .roundedRect
+        tf.font = UIFont.systemFont(ofSize: 14)
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        return tf
+    }()
+    
+    let passwordTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Password"
+        tf.isSecureTextEntry = true
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.borderStyle = .roundedRect
+        tf.font = UIFont.systemFont(ofSize: 14)
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        return tf
+    }()
+    
+    let signUpButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Sign Up", for: .normal)
+        button.backgroundColor = UIColor(red: 149/255, green: 204/255, blue: 244/255, alpha: 1)
+        button.layer.cornerRadius = 5
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.white, for: .normal)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view, typically from a nib.
+        view.addSubview(addPhotoButton)
+        
+        addPhotoButton.topAnchor.constraint(equalTo:  view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+        addPhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        addPhotoButton.heightAnchor.constraint(equalToConstant: 140).isActive = true
+        addPhotoButton.widthAnchor.constraint(equalToConstant: 140).isActive = true
+        
+        let vStackView = UIStackView(arrangedSubviews: [emailTextField,usernameTextField,passwordTextField,signUpButton])
+        vStackView.axis = .vertical
+        vStackView.distribution = .fillEqually
+        vStackView.spacing = 10
+        vStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(vStackView)
+        
+        vStackView.topAnchor.constraint(equalTo: addPhotoButton.bottomAnchor, constant: 20).isActive = true
+        vStackView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        vStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30).isActive = true
+        vStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true
     }
 
 
